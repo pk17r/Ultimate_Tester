589b65439b46dfe534aeddcef79deee7b4b1721c
Prashant Kumar <pkr@vt.edu>
2023-09-30 14:48:21 -0700

    removed USBV changes from main.c

--- ComponentTester-1.50m_ATmega1284p/Files/main.c
+++ ComponentTester-1.50m_ATmega1284p/Files/main.c
@@ -2173,107 +2173,109 @@ void ShowBattery(void)
   /* display battery info (text) */
   Display_EEString_Space(Battery_str);     /* display: Bat. */
 
-  #ifdef BAT_EXT_UNMONITORED
-  /* check for unmonitored external power supply */
-  if (Cfg.Vbat < 900)            /* < 0.9V */
-  {
-    /* powered by unmonitored external PSU */
-    /* low voltage caused by diode's leakage current */
+    #ifdef BAT_EXT_UNMONITORED
+    /* check for unmonitored external power supply */
+    if (Cfg.Vbat < 900)            /* < 0.9V */
+    {
+      /* powered by unmonitored external PSU */
+      /* low voltage caused by diode's leakage current */
 
-    /* display status */
-    #ifdef LCD_COLOR
-    UI.PenColor = COLOR_BAT_OK;       /* use OK color */
+      /* display status */
+      #ifdef LCD_COLOR
+      UI.PenColor = COLOR_BAT_OK;       /* use OK color */
+      #endif
+      Display_EEString(External_str);   /* display: ext */
+    }
+    else                           /* battery operation */
+    {
+      /* powered by battery */
     #endif
-    Display_EEString(External_str);   /* display: ext */
-  }
-  else                           /* battery operation */
-  {
-    /* powered by battery */
-  #endif
 
-    /* display battery voltage */
-    Display_Value(Cfg.Vbat / 10, -2, 'V');
-    Display_Space();
+      /* display battery voltage */
+      Display_Value(Cfg.Vbat / 10, -2, 'V');
+      Display_Space();
 
-    /* display status (ok, weak, low) */
-    if (Cfg.Vbat < BAT_LOW)           /* low level reached */
-    {
-      #ifdef LCD_COLOR
-      UI.PenColor = COLOR_BAT_LOW;    /* set LOW color */
-      #endif
-      Display_EEString(Low_str);      /* display: low */
+      /* display status (ok, weak, low) */
+      if (Cfg.Vbat < BAT_LOW)           /* low level reached */
+      {
+        #ifdef LCD_COLOR
+        UI.PenColor = COLOR_BAT_LOW;    /* set LOW color */
+        #endif
+        Display_EEString(Low_str);      /* display: low */
+      }
+      else if (Cfg.Vbat < BAT_WEAK)     /* warning level reached */
+      {
+        #ifdef LCD_COLOR
+        UI.PenColor = COLOR_BAT_WEAK;   /* set WEAK color */
+        #endif
+        Display_EEString(Weak_str);     /* display: weak */
+      }
+      else                              /* ok */
+      {
+        #ifdef LCD_COLOR
+        UI.PenColor = COLOR_BAT_OK;     /* set OK color */
+        #endif
+        Display_EEString(OK_str);       /* display: ok */
+      }
+
+    #ifdef BAT_EXT_UNMONITORED
     }
-    else if (Cfg.Vbat < BAT_WEAK)     /* warning level reached */
-    {
-      #ifdef LCD_COLOR
-      UI.PenColor = COLOR_BAT_WEAK;   /* set WEAK color */
-      #endif
-      Display_EEString(Weak_str);     /* display: weak */
-    }
-    else                              /* ok */
-    {
-      #ifdef LCD_COLOR
-      UI.PenColor = COLOR_BAT_OK;     /* set OK color */
-      #endif
-      Display_EEString(OK_str);       /* display: ok */
-    }
+    #endif
 
-  #ifdef BAT_EXT_UNMONITORED
-  }
-  #endif
-
-  #ifdef LCD_COLOR
-  UI.PenColor = COLOR_PEN;       /* set color back to default */
-  #endif
+    #ifdef LCD_COLOR
+    UI.PenColor = COLOR_PEN;       /* set color back to default */
+    #endif
 
   #endif
+
 
   #ifdef UI_BATTERY
-    /* display battery info (small symbol) */
+  /* display battery info (small symbol) */
 
-    /* get battery status */
-  #ifdef BAT_EXT_UNMONITORED
-  /* check for unmonitored external power supply */
+  /* get battery status */
+    #ifdef BAT_EXT_UNMONITORED
+    /* check for unmonitored external power supply */
     if (Cfg.Vbat < 900)                 /* < 0.9V */
     {
       /* powered by unmonitored external PSU */
       /* low voltage caused by diode's leakage current */
-  #ifdef LCD_COLOR
+      #ifdef LCD_COLOR
       UI.PenColor = COLOR_BAT_OK;       /* set OK color */
-  #endif
+      #endif
       Char1 = LCD_CHAR_BAT_LH;          /* left: high */
       Char2 = LCD_CHAR_BAT_RH;          /* right: high */
     }
     else
-  #endif
+    #endif
     if (Cfg.Vbat < BAT_LOW)             /* low level reached */
     {
-  #ifdef LCD_COLOR
+      #ifdef LCD_COLOR
       UI.PenColor = COLOR_BAT_LOW;      /* set LOW color */
-  #endif
+      #endif
       Char1 = LCD_CHAR_BAT_LL;          /* left: low */
       Char2 = LCD_CHAR_BAT_RL;          /* right: low */
     }
     else if (Cfg.Vbat < BAT_WEAK)       /* warning level reached */
     {
-  #ifdef LCD_COLOR
+      #ifdef LCD_COLOR
       UI.PenColor = COLOR_BAT_WEAK;     /* set WEAK color */
-  #endif
+      #endif
       Char1 = LCD_CHAR_BAT_LH;          /* left: high */
       Char2 = LCD_CHAR_BAT_RL;          /* right: low */
     }
     else                                /* ok */
     {
-  #ifdef LCD_COLOR
+      #ifdef LCD_COLOR
       UI.PenColor = COLOR_BAT_OK;       /* set OK color */
-  #endif
+      #endif
       Char1 = LCD_CHAR_BAT_LH;          /* left: high */
       Char2 = LCD_CHAR_BAT_RH;          /* right: high */
     }
-    /* display small battery symbol */
-    Display_Char(Char1);             /* display left part of symbol */
-    Display_Char(Char2);             /* display right part of symbol */
-    Display_Space();                 /* display space */
+
+  /* display small battery symbol */
+  Display_Char(Char1);             /* display left part of symbol */
+  Display_Char(Char2);             /* display right part of symbol */
+  Display_Space();                 /* display space */
 
     #ifdef LCD_COLOR
     UI.PenColor = COLOR_PEN;       /* set color back to default */
@@ -2316,49 +2318,49 @@ void CheckBattery(void)
   /* get current battery voltage */
   U_Bat = ReadU(TP_BAT);           /* read voltage (mV) */
 
-#ifdef BAT_DIVIDER
-    uint32_t          Temp;          /* temporary value */
+  #ifdef BAT_DIVIDER
+  uint32_t          Temp;          /* temporary value */
 
-    /*
-     *  ADC pin is connected to a voltage divider (top: R1 / bottom: R2).
-     *  - U2 = (Uin / (R1 + R2)) * R2
-     *  - Uin = (U2 * (R1 + R2)) / R2
-     */
+  /*
+   *  ADC pin is connected to a voltage divider (top: R1 / bottom: R2).
+   *  - U2 = (Uin / (R1 + R2)) * R2 
+   *  - Uin = (U2 * (R1 + R2)) / R2
+   */
 
-    Temp = (((uint32_t)(BAT_R1 + BAT_R2) * 1000) / BAT_R2);   /* factor (0.001) */
-    Temp *= U_Bat;                   /* Uin (0.001 mV) */
-    Temp /= 1000;                    /* Uin (mV) */
-    U_Bat = (uint16_t)Temp;          /* keep 2 bytes */
-#endif
+  Temp = (((uint32_t)(BAT_R1 + BAT_R2) * 1000) / BAT_R2);   /* factor (0.001) */
+  Temp *= U_Bat;                   /* Uin (0.001 mV) */
+  Temp /= 1000;                    /* Uin (mV) */
+  U_Bat = (uint16_t)Temp;          /* keep 2 bytes */
+  #endif
 
   U_Bat += BAT_OFFSET;             /* add offset for voltage drop */
   Cfg.Vbat = U_Bat;                /* save battery voltage */
   Cfg.BatTimer = 100;              /* reset timer for next battery check (in 100ms) */
-  /* about 10s */
+                                   /* about 10s */
 
   /* check for low-voltage situation */
   if (U_Bat < BAT_LOW)             /* low level reached */
   {
-#ifdef BAT_EXT_UNMONITORED
+    #ifdef BAT_EXT_UNMONITORED
     /* not for unmonitored external power supply */
     if (U_Bat >= 900)                   /* >= 0.9V */
     {
       /* battery operation */
-#endif
+    #endif
 
-#ifdef UI_COLORED_CURSOR
-/* because of TestKey() we have to reset the pen color */
+      #ifdef UI_COLORED_CURSOR
+      /* because of TestKey() we have to reset the pen color */
       UI.PenColor = COLOR_PEN;     /* set default pen color */
-#endif
+      #endif
 
       LCD_Clear();                 /* clear display */
       ShowBattery();               /* display battery status */
       MilliSleep(3000);            /* let user read info */
       PowerOff();                  /* power off */
 
-#ifdef BAT_EXT_UNMONITORED
+    #ifdef BAT_EXT_UNMONITORED
     }
-#endif
+    #endif
   }
 }
 
