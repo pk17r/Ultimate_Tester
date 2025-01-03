d1fdd27df85f6b28e38ef742de59d8662ea55516
Prashant Kumar <prash.k20@gmail.com>
2025-01-02 22:27:09 -0800

    Add license for INA226.c

--- ComponentTester-1.50m/Files/INA226.c
+++ ComponentTester-1.50m/Files/INA226.c
@@ -5,12 +5,35 @@
 // PURPOSE: Arduino library for INA226 power sensor
 //     URL: https://github.com/RobTillaart/INA226
 //
-//  ADAPTED TO COMPONENT TESTED PROJECT
-//      By: Prashant Kumar
+//  ADAPTED TO COMPONENT TESTER PROJECT
+//      By: Prashant Kumar    https://github.com/pk17r
 //    DATE: 2024-12-23
 //
 //  Read the datasheet for the details
-
+//  
+//  
+//  MIT License     (Date 2025-01-02)
+//  
+//  Copyright (c) 2021-2024 Rob Tillaart
+//  
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//  
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//  
 
 // #define INA226_LIB_VERSION              "0.6.0"
 
